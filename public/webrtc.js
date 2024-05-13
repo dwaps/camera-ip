21ed60635c165fc861bdcb624e62a96a97dba04f
michael <ei.dwaps@gmail.com>
2024-05-13 07:54:50 +0200

    refacto style

--- public/webrtc.js
+++ public/webrtc.js
@@ -1,6 +1,17 @@
 const socket = io();
 
-function requestReloadWindow() {
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+}
+
+function requestReloadWindow(e) {
+  e.preventDefault();
   socket.emit("reload");
 }
 
